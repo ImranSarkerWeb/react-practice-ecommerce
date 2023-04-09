7d05077bd0c8df904e5d58610e2ec5f20c05acde
imransarkerweb <imransarker.web@gmail.com>
2023-04-09 23:46:46 +0600

    item style added

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./ReviewItem.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 const ReviewItem = ({ product }) => {
   console.log(product);
@@ -7,6 +9,18 @@ const ReviewItem = ({ product }) => {
   return (
     <div className="review-item">
       <img src={img} alt="" />
+      <div className="item-details">
+        <p>{name}</p>
+        <p>
+          Price: <span className="text-orange">${price}</span>
+        </p>
+        <p>
+          Order Quantity: <span className="text-orange">${quantity}</span>
+        </p>
+      </div>
+      <button className="delet-btn">
+        <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
+      </button>
     </div>
   );
 };
