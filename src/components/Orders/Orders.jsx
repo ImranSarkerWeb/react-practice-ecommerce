4c70ea594d1e770e3ae1b0447bdd92d2479a1181
imransarkerweb <imransarker.web@gmail.com>
2023-04-09 23:19:03 +0600

    individual order item added in order page

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Cart from "../Cart/Cart";
 import "./Orders.css";
 import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
   const cart = useLoaderData();
   return (
     <div className="orders">
-      <div className="products">Products here {cart.length}</div>
+      <div className="items">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
+      </div>
       <div>
         Order summery
         <Cart items={cart}></Cart>
