547002bcc131101e304e77a2027ce7127b05a9ba
imransarkerweb <imransarker.web@gmail.com>
2023-04-09 22:52:29 +0600

    data shared using custom loader

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Cart from "../Cart/Cart";
 import "./Orders.css";
+import { useLoaderData } from "react-router-dom";
 
 const Orders = () => {
+  const cart = useLoaderData();
   return (
     <div className="orders">
-      <div className="products">Products here</div>
+      <div className="products">Products here {cart.length}</div>
       <div>
         Order summery
-        <Cart items={[]}></Cart>
+        <Cart items={cart}></Cart>
       </div>
     </div>
   );
